5957a35fe2718e16cf270feb95770e807c3fffa0
Kawthar Shaalan <101220311@mu.edu.lb>
2025-11-22 18:56:47 +0200

    did the connection folder

---
+++ server/connection/config.php
@@ -0,0 +1,6 @@
+<?php
+$env = parse_ini_file(__DIR__ . "/.env");
+$API_KEY=$env['OPENAI_KEY'];
+$URL_OPENAI="https://api.openai.com/v1/responses";
+$input = json_decode(file_get_contents('php://input'), true);
+?>
---
+++ server/connection/connection.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header(header: "Content-Type: application/json");
+
+$connection = new mysqli ("localhost", "root","","chat", 3310);
+
+if($connection->connect_error)
+{
+    die("connection error:".$connection->connect_error);
+}
+
+?>
